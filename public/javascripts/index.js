0d57c40bf86fc562695535de5b6d6784e5f2af3c
Archawin <zepalzth@gmail.com>
2018-11-26 23:50:13 +0700

    Fix show all resolves

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,38 +1,57 @@
 $(() => {
-  $('#dig').on('click', () => {
-    $.get("http://localhost:3000/dig/" + $('#url').val(),
-      (data) => {
-        $('#text').html('')
-        $('#text').append(data)
-      });
+  const initTable = () => {
+    $('#table').html(`
+      <thead class="thead-dark">
+        <tr>
+          <th scope="col">TYPE</th>
+          <th scope="col">DOMAIN NAME</th>
+          <th scope="col">RESOLVE</th>
+        </tr>
+      </thead>
+      <tbody id="body"></tbody>`)
+  }
+
+  const createRow = (rrtype, url, data) => {
+    return `<tr>
+        <th scope="row">${rrtype}</th>
+        <td>${url}</td> 
+        <td>${getValue(rrtype, data)}</td>
+      </tr>`
+  }
+
+  const getValue = (rrtype, data) => {
+    let value = data
+    switch (rrtype) {
+      case 'A':
+        value = (`<a target="_blank" rel="noopener noreferrer" href='http://${data}'>${data}</a>`)
+        break;
+      case 'MX':
+        value = data.exchange + " with priority " + data.priority
+        break;
+      case 'SOA':
+        value = Object.keys(data).map(key => `<div>${key}: ${data[key]}</div>`).join('')
+        break;
+      default:
+        break;
+    }
+    return value
+  }
+
+  $('#lookup').on('click', async () => {
+    const url = $('#url').val()
+    const data = await $.get("http://localhost:3000/lookup/" + url)
+    initTable()
+    console.log(data);
+
+    data.map(resolve => {
+      if (Array.isArray(resolve.data)) {
+        resolve.data.map(item => {
+          $('#body').append(createRow(resolve.rrtype, url, item))
+        })
+      } else {
+        $('#body').append(createRow(resolve.rrtype, url, resolve.data))
+      }
+    })
   })
 
-  $('#lookup').on('click', () => {
-    $.get("http://localhost:3000/lookup/" + $('#url').val(),
-      (data) => {
-        $('#text').html('')
-        data.map(server => {
-          $('#text').append(`<div>Address: ${server.address} IPv${server.family}</div>`)
-        })
-      });
-  })
-
-  $('#reverse').on('click', () => {
-    $.get("http://localhost:3000/reverse/" + $('#url').val(),
-      (data) => {
-          $('#body').html('')
-          data.map(resolve => {
-          if(resolve) {
-            resolve.data.map(item => {
-                $('#body').append(`
-                  <tr>
-                    <th scope="row">${resolve.rrtype}</th>
-                    <td>${$('#url').val()}</td> 
-                    <td>${item}</td>
-                  </tr>`)
-            })
-          }
-        })
-      });
-  })
 })
--- routes/index.js
+++ routes/index.js
@@ -1,35 +1,24 @@
 var express = require('express');
-var execa = require('execa');
 const dns = require('dns');
 const dnsPromises = dns.promises;
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/dig/:url', async(req, res) => {
-  const result = await execa.shell('dig ' + req.params.url)
-  res.send(result.stdout)
-})
-
-router.get('/lookup/:url', async(req, res) => {
-  dns.lookup(req.params.url, {all: true} ,(err, addresses) => {
-    res.send(addresses)
-  });
-})
-
-router.get('/reverse/:ip', async (req, res) => {
-  const rrtypes = [ 'A', 'AAAA', 'CNAME', 'MX', 'NAPTR', 'NS', 'PTR', 'SOA', 'SRV', 'TXT']
-  const resolves = await Promise.all(rrtypes.map(async rrtype => {    
-    try{ 
-      const data = await dnsPromises.resolve(req.params.ip, rrtype)      
-      return {data, rrtype}
-    }catch(error) {
+router.get('/lookup/:ip', async (req, res) => {
+  const rrtypes = ['A', 'AAAA', 'CNAME', 'MX', 'NAPTR', 'NS', 'PTR', 'SOA', 'SRV', 'TXT']
+  const resolves = await Promise.all(rrtypes.map(async rrtype => {
+    try {
+      const data = await dnsPromises.resolve(req.params.ip, rrtype)
+      return { data, rrtype }
+    } catch (error) {
       console.log(error)
+      return { data: "NO RECORD FOUND", rrtype }
     }
-  }))  
+  }))
   res.send(resolves)
 })
 
